32429ec57b500e3448f9157861ccacb317f25270
jv <jv@jv.local>
2026-07-15 23:37:16 +0530

    Footer: style X icon with black background and white logo for alignment and consistency

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -81,7 +81,7 @@ export const Footer: React.FC = () => {
                 <svg style={{ width: "20px", height: "20px" }} className="fill-current" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.779-1.75-1.75s.784-1.75 1.75-1.75 1.75.779 1.75 1.75-.784 1.75-1.75 1.75zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
               </a>
               {/* X (Twitter) */}
-              <a href="#" style={{ width: "42px", height: "42px", borderRadius: "10px", background: "white", display: "flex", alignItems: "center", justifyContent: "center", color: "black", flexShrink: 0 }} className="hover:scale-105 transition-transform duration-200">
+              <a href="#" style={{ width: "42px", height: "42px", borderRadius: "10px", background: "black", display: "flex", alignItems: "center", justifyContent: "center", color: "white", flexShrink: 0 }} className="hover:scale-105 transition-transform duration-200">
                 <svg style={{ width: "20px", height: "20px" }} className="fill-current" viewBox="0 0 24 24"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" /></svg>
               </a>
             </div>
